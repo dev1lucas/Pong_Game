86d4702c30ff1cab41f1daf13c5b741632d65621
devlucas_s <105252290+dev1lucas@users.noreply.github.com>
2023-07-27 12:52:49 -0300

    atualização do main.lua

--- main.lua
+++ main.lua
@@ -1,36 +1,6 @@
---[[
-    GD50 2018
-    Pong Remake
-
-    pong-9
-    "The Serve Update"
-
-    -- Main Program --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
-
--- push is a library that will allow us to draw our game at a virtual
--- resolution, instead of however large our window is; used to provide
--- a more retro aesthetic
---
 -- https://github.com/Ulydev/push
 push = require 'push'
 
--- the "Class" library we're using will allow us to represent anything in
--- our game as code, rather than keeping track of many disparate variables and
--- methods
---
 -- https://github.com/vrld/hump/blob/master/class.lua
 Class = require 'class'
 
